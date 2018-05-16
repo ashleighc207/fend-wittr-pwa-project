abe6922e08c7b1daf0916544b9c936b9de5f6ddc
Ashleigh Christopher <ashleighc207@gmail.com>
2018-05-15 21:05:12 -0400

    Return gif on 404 error

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
 		fetch(event.request).then(function(response){
 			if(response.status === 404) {
-				return new Response('You look like you are lost');
+				return fetch('/imgs/dr-evil.gif');
 			}
 			return response;
 		}).catch(function(){
