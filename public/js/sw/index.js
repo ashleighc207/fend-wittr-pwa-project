42cd030755ec30fbd0567918c36b6a479ef4580f
Ashleigh Christopher <Ashleigh@osxs-MBP.localdomain>
2018-05-15 20:41:27 -0400

    Add request interception to turn all jpg to gif of dr evil

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,5 +1,7 @@
 self.addEventListener('fetch', function(event) {
+	if (event.request.url.endsWith('.jpg')) {
   event.respondWith(
-fetch('/imgs/dr-evil.gif')
-  	);
+		fetch('/imgs/dr-evil.gif')
+  );
+}
 });
